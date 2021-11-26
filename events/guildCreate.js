07d27d320ee6e1e2d4d7f94a53364ef4f9eb19f9
robloxupdatechecker <robloxupdatechecker@gmail.com>
2021-11-26 01:18:29 +0000

    Update examples

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -20,12 +20,12 @@ module.exports = {
 This bot uses [slash commands](https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ).
 
 Users with the \`Administrator\` permission can get started by using the following commands:
-\`/moderation setupdatechannel <channel>\`
-\`/moderation setupdaterole <role>\`
-\`/moderation createrolemenu\`
+\`/setupdatechannel #channel\`
+\`/setupdaterole @role\`
+\`/createrolemenu\`
 Examples:
-\`/moderation setupdatechannel #roblox-updates\`
-\`/moderation setupdaterole @Roblox Update Pings\``)
+\`/setupdatechannel #roblox-updates\`
+\`/setupdaterole @Roblox Update Pings\``)
         guild.channels.cache.get(guild.systemChannelId || guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.permissionsFor(guild.me).has("SEND_MESSAGES")).id).send({
             embeds: [embed],
             components: [row]
