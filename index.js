ab681cee12cdf21e81eac8f8f50c318e5b3d6fcc
6GdmrpHL5MR9sbAH <robloxupdatechecker@gmail.com>
2022-03-08 11:21:47 +0000

    Remove mod logs Set guildCreate to use client.application.id

--- index.js
+++ index.js
@@ -203,14 +203,6 @@ Sent <t:${(Date.now() / 1000).toFixed(0)}:R>.`)
     })
 }
 
-fs.readdirSync("./events").filter(file => file.endsWith(".js")).forEach(file => {
-    const event = require(`./events/${file}`)
-
-    client.on(file.replace(".js", ""), (...args) => {
-        event.execute(client, ...args)
-    })
-})
-
 client.on("ready", _ => {
     console.log(`${client.user.tag} is online`)
 
@@ -389,6 +381,34 @@ client.on("interactionCreate", async interaction => {
                 break
         }
     }
+})
+
+client.on("guildCreate", guild => {
+    const row = new Discord.MessageActionRow()
+        .addComponents(
+            new Discord.MessageButton()
+                .setLabel("Invite Bot")
+                .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.application.id}&permissions=8&scope=bot%20applications.commands`)
+                .setStyle("LINK")
+        )
+
+    const embed = new Discord.MessageEmbed()
+        .setColor("BLUE")
+        .setDescription(`Hello, I am Roblox Update Checker. As the name suggests I check for Roblox updates.
+This bot uses [slash commands](https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ).
+
+Users with the \`Administrator\` permission can get started by using the following commands:
+\`/setupdatechannel #channel\`
+\`/setupdaterole @role\`
+\`/createrolemenu\`
+Examples:
+\`/setupdatechannel #roblox-updates\`
+\`/setupdaterole @Roblox Update Pings\``)
+
+    guild.channels.cache.get(guild.systemChannelId || guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.permissionsFor(guild.me).has("SEND_MESSAGES")).id).send({
+        embeds: [embed],
+        components: [row]
+    })
 })
 
 process.on("uncaughtException", console.error)
